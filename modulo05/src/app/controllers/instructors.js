b97ac323a4578baf817dd6b23d8da1daa0dccda0
Reinaldo <reinaldo800@gmail.com>
2021-06-29 17:21:36 -0300

    Membros Funcionando

--- modulo05/src/app/controllers/instructors.js
+++ modulo05/src/app/controllers/instructors.js
@@ -1,15 +1,13 @@
+const Instructor = require("../models/Instructor");
 const { age, date } = require("../../lib/utils");
-const db = require("../../config/db");
 
 module.exports = {
   // shorthand da linguagem
   index(req, res) {
-    db.query(`SELECT * FROM instructors`, function (err, results) {
-      if (err) {
-        return res.send("Database Error! Please review your application.");
-      }
-
-      return res.render("instructors/index", { instructors: results.rows });
+    Instructor.all(function (instructors) {
+      return res.render("instructors/index", {
+        instructors,
+      });
     });
   },
   create(req, res) {
@@ -22,52 +20,47 @@ module.exports = {
       if (req.body[key] == "") return res.send("Please, fill all fields");
     }
 
-    const query = `
-      INSERT INTO instructors(
-        name,
-        avatar_url,
-        gender,
-        services,
-        birth,
-        created_at
-      ) VALUES ($1, $2, $3, $4, $5, $6)
-      RETURNING id
-    `;
-    // $ placeholder
-
-    const values = [
-      req.body.name,
-      req.body.avatar_url,
-      req.body.gender,
-      req.body.services,
-      date(req.body.birth).iso,
-      date(Date.now()).iso,
-    ];
-
-    db.query(query, values, function (err, results) {
-      if (err) {
-        return res.send("Database Error! Please review your application.");
-      }
-
-      return res.redirect(`/instructors/${results.rows[0].id}`);
+    Instructor.create(req.body, function (instructor) {
+      return res.redirect(`/instructors/${instructor.id}`);
     });
   },
   show(req, res) {
-    return;
+    Instructor.find(req.params.id, function (instructor) {
+      if (!instructor) return res.send("Instructor not found!");
+
+      instructor.age = age(instructor.birth);
+      instructor.services = instructor.services.split(",");
+
+      instructor.created_at = date(instructor.created_at).format;
+
+      return res.render("instructors/show", { instructor });
+    });
   },
   edit(req, res) {
-    return;
+    Instructor.find(req.params.id, function (instructor) {
+      if (!instructor) return res.send("Instructor not found!");
+
+      instructor.birth = date(instructor.birth).iso;
+
+      return res.render("instructors/edit", { instructor });
+    });
   },
   put(req, res) {
     const keys = Object.keys(req.body);
 
     for (key of keys) {
-      if (req.body[key] == "") return res.send("Please, fill all fields");
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields!");
+      }
     }
 
-    return;
+    Instructor.update(req.body, function () {
+      return res.redirect(`/instructors/${req.body.id}`);
+    });
   },
   delete(req, res) {
-    return;
+    Instructor.delete(req.body.id, function () {
+      return res.redirect(`/instructors`);
+    });
   },
 };
